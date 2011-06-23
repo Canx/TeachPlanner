cb82c66f2c8a212515595d9c1f01d0e02c170e34
Canx <canchete@gmail.com>
2011-06-23 19:07:39 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachPlanner::Application.config.secret_token = '241e9d73abe1ad92684792fd559ef12ecdec8bc0227c86b18599103eb644315dc8e5c024a2238240c737dcfb858a2bc3b0a3ba01d83a2a59aed12fb3fe5bbe1f'
